4c9708cffd3e8cdf6ba045789293f4c8f5faf342
Mina Ezzat <53598947+MeNa619493@users.noreply.github.com>
2023-01-11 15:16:21 +0200

    add association ui

---
+++ app/src/main/java/com/gm3ya/gm3ya/features/notification/NotificationFragment.kt
@@ -0,0 +1,12 @@
+package com.gm3ya.gm3ya.features.notification
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.gm3ya.gm3ya.R
+
+class NotificationFragment : Fragment() {
+
+}
