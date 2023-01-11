c0c5340399f71611953e608dab8e056f55ffc757
Your Name <mostafa.ahabdullah@orange.com>
2023-01-11 15:10:52 +0200

    create association model and handle tabs

--- app/src/main/java/com/gm3ya/gm3ya/common/firebase/data/UserModel.kt
+++ app/src/main/java/com/gm3ya/gm3ya/common/firebase/data/UserModel.kt
@@ -1,6 +1,5 @@
 package com.gm3ya.gm3ya.common.firebase.data
 
-import android.net.Uri
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
@@ -16,5 +15,42 @@ data class UserModel(
 enum class UserState(val value:String){
     Pending("Pending"),Accepted("Accepted"),Rejected("Rejected")
 }
+
+@Parcelize
+data class AssociationModel(
+    val users:List<UserModel>?=null,
+    val startDate:String? = null,
+    val endDate:String? = null,
+    val hashed:String?=null,
+    val state:String?= null,
+    val maxSize:Int?=null,
+    val amountPerMonth:String?=null,
+    val months:List<MonthModel>?=null,
+    val totalAmount:String?=null
+): Parcelable
+
+
+enum class AssociationState(val value:String){
+    Available("Available"),Completed("Completed"),Ongoing("Ongoing"),Finished("Finished")
+}
+
+@Parcelize
+data class MonthModel(
+    val name:String?=null,
+    val paidMonths:List<PaidMonthModel>?=null
+): Parcelable
+
+
+@Parcelize
+data class PaidMonthModel(
+    val userId:String?=null,
+    val userName:String?=null,
+    val state:Boolean?=null
+): Parcelable
+
+
+
+
+
 
 
--- app/src/main/java/com/gm3ya/gm3ya/features/associationsdetails/AssociationsDetailsFragment.kt
+++ app/src/main/java/com/gm3ya/gm3ya/features/associationsdetails/AssociationsDetailsFragment.kt
@@ -1,16 +1,10 @@
 package com.gm3ya.gm3ya.features.associationsdetails
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
-import com.gm3ya.gm3ya.R
 import com.gm3ya.gm3ya.common.base.AnyViewModel
 import com.gm3ya.gm3ya.common.base.BaseFragment
-import com.gm3ya.gm3ya.databinding.FragmentAllAccountsBinding
 import com.gm3ya.gm3ya.databinding.FragmentAssociationsDetailsBinding
+import com.google.android.material.tabs.TabLayout
 
 class AssociationsDetailsFragment : BaseFragment<FragmentAssociationsDetailsBinding, AnyViewModel>(){
     override fun initBinding() = FragmentAssociationsDetailsBinding.inflate(layoutInflater)
@@ -21,8 +15,26 @@ class AssociationsDetailsFragment : BaseFragment<FragmentAssociationsDetailsBind
 
     override fun onFragmentCreated() {
 
-        binding.rvAssociations.adapter = DetailAssociationAdapter(listOf("ahmed","alaa","ahmed","alaa","ahmed","alaa")){
+        binding.apply {
+            list.forEach {
+                tabLayout.addTab(tabLayout.newTab().setId(1).setText(it))
+            }
 
+            rvAssociations.adapter = DetailAssociationAdapter(listOf("ahmed","alaa","ahmed","alaa","ahmed","alaa")){
+
+            }
+
+            tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+                override fun onTabSelected(tab: TabLayout.Tab?) {
+                   showErrorMsg("${tab?.id} +  ${tab?.text}")
+                }
+                override fun onTabUnselected(tab: TabLayout.Tab?) {}
+                override fun onTabReselected(tab: TabLayout.Tab?) {}
+            })
         }
+
     }
 }
+
+
+val list = listOf("mostafa","mina","alaa","mostafa","mina","alaa","mostafa","mina","alaa","mostafa","mina","alaa")
