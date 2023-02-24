2cd7a14383a7460aa09b78585492401b7c0fc668
Mina Ezzat <53598947+MeNa619493@users.noreply.github.com>
2023-02-24 13:28:31 +0200

    alert dialog

--- app/src/main/java/com/gm3ya/gm3ya/features/customalertdialog/CustomAlertDialog.kt
+++ app/src/main/java/com/gm3ya/gm3ya/features/customalertdialog/CustomAlertDialog.kt
@@ -15,7 +15,7 @@ class CustomAlertDialog : BaseFragmentDialog<CustomAlertDialogBinding>() {
     private fun observeOkButton() {
         binding.btnOk.setOnClickListener {
             dialog?.dismiss()
-            findNavController().popBackStack()
+
         }
     }
 }
